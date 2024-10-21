3d11f2ac4f9a4b81b51f644b7bf3f2eb1966a393
ludyna <sikachyna.n@gmail.com>
2024-10-21 15:41:22 +0300

    Defined bot messages

---
+++ bot/src/messages.rs
@@ -0,0 +1,7 @@
+pub const START: &str = "Hello! This bot is made to check your password entropy";
+
+pub const INFO: &str = "Source code: {source_link}\nSupport us: {donate_link}";
+
+pub const REQUIRE_PASSWORD: &str = "Send me your password";
+
+pub const RESULT: &str = "Recomended entropy:\n70\nYour password entropy\n{entropy}"
